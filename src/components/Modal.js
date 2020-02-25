d2fde45db6e68b2655393329889e301db1cb78ea
olivertylsar <kendees@seznam.cz>
2020-02-25 16:14:02 +0100

    modal props destructured

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-function Modal(props) {
+function Modal({ onSubmitDone }) {
   return (
     <div className='Modal'>
       <div className='Modal__content'>
         <h2 className='Modal__heading'>Odesláno.</h2>
         <p className='Modal__paragraph'>Skvělý tým, díky!</p>
-        <button
-          className='btn btn--done Modal__button'
-          onClick={props.onSubmitDone}
-        >
+        <button className='btn btn--done Modal__button' onClick={onSubmitDone}>
           Hotovo
         </button>
       </div>
